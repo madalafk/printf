82404280ad299d1fe0a92f0ef89768322f18634a
Attah247 <122897749+Attah247@users.noreply.github.com>
2023-03-28 23:16:54 +0100

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -11,7 +11,7 @@
 * @size: Size specifier
 * Return: Number of chars printed.
 */
-int print_unsigned(va_list types, char buffer[],
+int print_unsigned(va_list types, char buffer)
 int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
